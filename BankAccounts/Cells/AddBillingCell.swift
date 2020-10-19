ac071679f69a4314f5409c26ae5a0745955b4ee8
Dmitry Makarevich <d.a.makar@list.ru>
2020-10-19 23:17:51 +0300

    add extension UIView for corner radius

--- BankAccounts/Cells/AddBillingCell.swift
+++ BankAccounts/Cells/AddBillingCell.swift
@@ -7,17 +7,7 @@
 
 import UIKit
 
-@IBDesignable
 class AddBillingCell: UICollectionViewCell {
     static let reuseIdentifier: String = "AddBillingCell"
-    
-    @IBInspectable
-    private var cornerRadius: CGFloat {
-        get {
-            return self.layer.cornerRadius
-        }
-        set {
-            self.layer.cornerRadius = newValue
-        }
-    }
+
 }
--- BankAccounts/Cells/BillingCell.swift
+++ BankAccounts/Cells/BillingCell.swift
@@ -7,22 +7,11 @@
 
 import UIKit
 
-@IBDesignable
 class BillingCell: UICollectionViewCell {
     static let reuseIdentifier: String = "BillingCell"
 
     @IBOutlet weak var balanceLabel: UILabel!
     @IBOutlet weak var ownerLabel: UILabel!
-
-    @IBInspectable
-    private var cornerRadius: CGFloat {
-        get {
-            return self.layer.cornerRadius
-        }
-        set {
-            self.layer.cornerRadius = newValue
-        }
-    }
 
     func set(balance: String, owner: String) {
         self.balanceLabel.text = balance
