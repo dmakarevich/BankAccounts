1daa6052f77708cb2df9b1509c462721ea2aee30
Dmitry Makarevich <d.a.makar@list.ru>
2020-11-02 21:12:57 +0300

    add constants file

---
+++ BankAccounts/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  BankAccounts
+//
+//  Created by Dmitry Makarevich on 29.10.2020.
+//
+
+import Foundation
+
+struct Constants {
+    static let billingModalVCIdentifier: String = "BillingModalViewController"
+}
